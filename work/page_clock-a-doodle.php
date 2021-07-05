676e0e1d409cc7ce796b7ddb31e82d320bae3eae
Q Manning <me@qmanning.com>
2021-07-05 17:33:52 -0500

    Clock-a-Doodle Case Study

---
+++ work/page_clock-a-doodle.php
@@ -0,0 +1,70 @@
+<?php
+/**
+* Template Name: Work - Clock-a-Doodle
+*
+*/
+?>
+
+<?php get_header(); ?>
+<link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/work/assets/css/clock-a-doodle.css" type="text/css" />
+
+	<div class="case-study-single">
+		<div id="top">
+			<div class="clock"><img src="http://res.cloudinary.com/rocksauce-studios/image/upload/v1420581638/yvxslcafug5w14mp9vn5.png"/></div>
+			<div class="description">
+				<table class="projectDescription">
+					<tr>
+						<td class="logo"></td>
+						<td class="text">
+							<p>
+								<h1>Your Morning Wake-Up Video</h1>
+								<p>
+									Waking up has never been so much fun! Choose a video from your phone or YouTube and actually look forward to your daily alarm.
+								</p>
+							</p>
+						</td>
+						<td class="alarmIcon"><img src="http://res.cloudinary.com/rocksauce-studios/image/upload/v1420581639/l2i7rjx7ixcrqfu8slhe.png" /></td>
+					</tr>
+				</table>
+			</div>
+		</div>
+		<div class="bigPhoto">
+			<div class="text">
+				<h1>EASY TO SET-UP &amp; A JOY TO USE</h1>
+				<p>Clock-A-Doodle's goal was to give users something to look forward to, rather than the annoying blaring of typical alarms.</p>
+
+				<p>So we made it simple to use. Add an alarm, choose a video, then go to sleep. Yeah, it's that easy. Pick from included options or upgrade to choose anything from YouTube.  </p>
+			</div>
+		</div>
+		<div class="multiColor">
+			<div class="text">
+				<h1>THE BONES &amp; MUSCLES</h1>
+				<p>Building a project means starting with the right bones & muscles, ensuring that all the joints connect as they're supposed to. </p>
+			</div>
+			<div class="appMap"></div>
+			<div class="logosAndMore">
+				<div class="text">
+					<h1>HOW TO NAME YOUR ROOSTER</h1>
+					<p>Often, we have the concept for a name, though we haven't quite decided on the exact words. Originally our client came to us with the name, “Video Rooster,” but after several logo versions, we realized it was just too on the nose. Along the way, we realized the perfect name was staring us right in the face: Clock-A-Doodle.</p>
+					<div class="logoList">
+						<div class="logo"><img src="http://res.cloudinary.com/rocksauce-studios/image/upload/v1420581639/d5kcd5ldddaw1e1axlst.png" /></div>
+						<div class="logo"><img src="http://res.cloudinary.com/rocksauce-studios/image/upload/v1420581639/tpl0xcopwdc7lu6ka3gw.png" /></div>
+						<div class="logo"><img src="http://res.cloudinary.com/rocksauce-studios/image/upload/v1420581638/yjyftq0ohpqzprvadez0.png" /></div>
+						<div class="logo"><img src="http://res.cloudinary.com/rocksauce-studios/image/upload/v1420581638/kiwzyrcwjqbemitmetal.png" /></div>
+						<div class="logo"><img src="http://res.cloudinary.com/rocksauce-studios/image/upload/v1420581638/rrskcpdlyhypa5kvxrm5.png" /></div>
+						<div class="logo"><img src="http://res.cloudinary.com/rocksauce-studios/image/upload/v1420581640/aoe5sdwkeoxzuvvtehyy.png" /></div>
+						<div class="bigLogo"><img src="http://res.cloudinary.com/rocksauce-studios/image/upload/v1420581638/k45v1awt1dddbbyc0mtu.png" /></div>
+					</div>
+				</div>
+				<div class="devices">
+					<div class="nexus9"></div>
+					<div class="iphone6gold"></div>
+					<div class="samsungGalaxy"></div>
+					<div class="iphone5"></div>
+					<div class="iphone6black"></div>
+					<div class="onePlus"></div>
+				</div>
+			</div>
+		</div>
+	</div>
+<?php get_footer(); ?>
