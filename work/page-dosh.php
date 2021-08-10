4f3ea7dfba0dee6503313f2310b076e105e8a08b
mojayaute <colorrr@gmail.com>
2021-08-10 16:03:47 -0500

    dosh page

---
+++ work/page-dosh.php
@@ -0,0 +1,145 @@
+<?php
+/**
+* Template Name: Work - Dosh
+*
+*/
+?>
+
+<?php get_header(); ?>
+<link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/work/dosh/assets/css/dosh_cs.css?version=<?php echo rand(1, 2000) ?>" type="text/css" />
+<div class="case-study-single">
+	<div id="doshTopLayer">
+		<div class="insideBounds">
+				<div class="psBounds">
+					<div class="psText">
+						<div class="textMove">
+							<div class="psLogo" alt="DOSH Logo"></div>
+							<h2>Cash back, automatically.</h2>
+							<p>
+								The #1 shopping app on iTunes gives you immediate cash back on purchases, hotels, rentals, online items and more. 
+							</p>
+							<ul id="dutyList">
+								<li>STRATEGY</li>
+								<li>UX</li>
+								<li>BRANDING</li>
+								<li>DESIGN</li>
+								<li>DEV</li>							
+							</ul>
+							<a href="https://link.dosh.cash/YCLI/vZKppBGhDJ" target="_blank" alt="DOWNLOAD DOSH" id="doshButton">
+								<svg viewBox="0 0 177 40" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+								    <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd" transform="translate(-203.000000, -499.000000)">
+								        <g transform="translate(204.000000, 500.000000)">
+								            <g>
+								                <g class="strokeIt" stroke-linecap="round" stroke-linejoin="round" stroke-width="2">
+								                    <g>
+								                        <polyline points="126.994434 38.0002 0.078998773 38.0002 0.078998773 0.0002 174.999244 0.0002 174.999244 38.0002 164.785845 38.0002"></polyline>
+								                    </g>
+								                    <path d="M131,38 L161,38" id="Path-4"></path>
+								                </g>
+								                <path d="M33.797546,0.682617188 L33.797546,8.89001465" id="Path-3" class="strokeIt" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
+								                <path d="M33.797546,12.6799927 L33.797546,15.8900146" id="Path-3-Copy" class="strokeIt" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
+								                <path d="M33.797546,19.6799927 L33.797546,37.8900146" id="Path-3-Copy-2" class="strokeIt" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
+								                <text id="DOWNLOAD-DOSH">
+								                    <tspan x="46.736773" y="23">DOWNLOAD DOSH</tspan>
+								                </text>
+								            </g>
+								            <g transform="translate(11.000000, 10.000000)" class="fillIt">
+								                <g transform="translate(0.825851, 0.220116)">
+								                    <path d="M9.22842342,2.51465815 C8.14372401,1.68300727 7.01259516,1.20311204 5.58337583,1.06564019 C5.57462826,1.06492232 5.56789935,1.0573847 5.56789935,1.0480524 L5.56789935,0.909503746 C5.56789935,0.554158741 5.35829398,0.217837256 5.03530656,0.130975144 C4.56428324,0.0046302531 4.13800714,0.3865364 4.13800714,0.869303099 L4.13800714,1.04841134 C1.73175087,1.27489891 0.0306838047,2.94394363 0.0306838047,5.15713283 L0.0306838047,5.20056388 C0.0306838047,7.01677169 0.834787897,8.10541957 2.53484563,8.85702809 C2.59742444,8.87712842 2.83529122,8.94389021 3.0055325,8.8591817 L4.87751375,7.9270292 C5.0073816,7.86313888 4.87785019,7.8444743 4.86237371,7.84052602 C2.20344708,7.15783289 1.63485465,6.38971338 1.63485465,5.04945252 L1.63485465,5.00566253 C1.63485465,3.64099414 2.81241294,2.55808921 4.65983368,2.55808921 C5.97970831,2.55808921 7.13707989,2.94789191 8.33516134,3.87932655 C8.43643136,3.95685637 8.53803782,4.00100529 8.65444786,4.02002881 C9.06827549,4.08714953 9.48546758,3.79964312 9.56049486,3.36030748 C9.62912969,2.95758314 9.41548697,2.66433378 9.22842342,2.51465815"></path>
+								                </g>
+								                <path d="M8.28443829,8.83522255 C8.19797187,8.81045608 7.99374962,8.76558928 7.84167638,8.84024763 L5.89635007,9.8086525 C5.8734718,9.8201384 5.87885492,9.85603183 5.90375187,9.86213372 C8.65183648,10.5204193 9.32338116,11.314741 9.32338116,12.6772558 L9.32338116,12.7206869 C9.32338116,14.2152895 8.06440312,15.2985534 6.15608579,15.2985534 C4.34870203,15.2985534 3.00897713,14.7134904 1.66891579,13.5002923 C1.56730934,13.392253 1.38461958,13.3053909 1.18140666,13.3053909 C0.755130558,13.3053909 0.409937755,13.6736576 0.409937755,14.1503224 C0.409937755,14.4320858 0.531730927,14.6704183 0.714420685,14.7999936 C1.99594055,15.8868468 3.37402021,16.5401073 4.94925676,16.7529553 C4.95766789,16.7540321 4.9637239,16.7615698 4.9637239,16.7705431 L4.9637239,16.9973896 C4.9637239,17.3574008 5.17534795,17.700542 5.5030456,17.7852505 C5.97137736,17.9062114 6.39361612,17.525741 6.39361612,17.0451279 L6.39361612,16.8251011 C9.05826232,16.7088064 10.9477387,15.0534012 10.9477387,12.5473216 L10.9477387,12.5042495 C10.9477387,10.7375746 10.0918221,9.60226525 8.28443829,8.83522255"></path>
+								            </g>
+								            <path d="M33.797546,19.6799927 L33.797546,37.8900146" class="strokeIt" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
+								        </g>
+								    </g>
+								</svg>
+							</a>
+						</div>						
+					</div>
+					<div class="sliderGraphic">
+						<img class="sliderImage" src="http://res.cloudinary.com/rocksauce-studios/image/upload/v1515782150/tr87kcakd36vyrde2hux.png" alt="DOSH iPhone Example">
+					</div>
+				</div>
+			</div>
+		</div>		
+	</div>
+	<!-- Case Study Content -->
+	<div class="doshContent">
+		<!-- Concept -->
+			<h2>Concept</h2>
+			<p>Each year, companies spend billions of dollars on marketing, trying to engender brand-loyalty. What if that money went directly to the people who shop there?</p>
+			<p>How much loyalty would the earn, and more importantly, how many people would it help? This is the heart of DOSH.</p>
+
+		<!-- What Rocksauce Did -->
+			<h2>What Rocksauce Did</h2>
+			<p>Rocksauce Studios was there since the beginning. We worked with company founders Ryan Wuerch and Ed Mock on initial concepts for Dosh starting in 2014. We worked with their team to define the initial product, strategy, UX, branding, UI, and development. Rocksauce and Dosh developed a partnership that stands strong.</p>
+			<div id="whatRocksauceDidImage" alt="Strategy, Ideation, User Experience, Branding, Design, Development"></div>
+
+		<!-- Branding -->
+			<h2>Branding</h2>
+			<p>Dosh went through two distinct branding stages. Our original brand designs in 2014 were created for a different, older demographic, at a time when design trends had just started to transition from skeumorphism. As the final product came into focus, and with it, the initial feature-set we would launch in the BETA MDP product, it was clear the brand needed some refreshing.</p>
+			<p>Purple has always been the key color, signifying youth, energy, and luxury. To support this, we deployed a fun-set of custom icons for various app functions, a bold, youthful color palette, and a set of gradients to be deployed throughout.</p>
+			<div id="brandingImage">
+				<img src="http://res.cloudinary.com/rocksauce-studios/image/upload/v1515782150/xhgbfrcm6epnh20z6gcy.png" alt="Dosh Branding Elements" />
+			</div>
+			
+		<!-- Marketing -->
+			<h2>Marketing</h2>
+			<p>To build hype for the app, Rocksauce built a "countdown" site, allowing users to initially sign-up to become part of the initial Dosh beta. Over 10,000 people registered less than two months. We consider it a success.</p>
+			
+			<div class="webScreens">
+				<img src="http://res.cloudinary.com/rocksauce-studios/image/upload/v1515782150/wu2dc51qaxd6jjensokq.jpg" alt="Dosh Countdown Homepage" />
+				<img src="http://res.cloudinary.com/rocksauce-studios/image/upload/v1515782150/zshzunblagqjjty9njrf.jpg" alt="Dosh Merchant Sign-Up" />
+				<img src="http://res.cloudinary.com/rocksauce-studios/image/upload/v1515782150/r9enobqa3ghjjsofr8ul.jpg" alt="Dosh About Dosh" />				
+			</div>
+
+		<!-- Mobile Apps -->
+			<h2>Mobile Apps</h2>
+			<p>The brilliance of Dosh is that users connect their cards via the app, and when they shop, they get cash back immediately from retailers nationwide. So, we obviously spent many hours planning the mobile app, from sign-up to wallet to booking a hotel for your upcoming trip.</p>
+
+			<div id="mobileAppEx">
+				<img src="http://res.cloudinary.com/rocksauce-studios/image/upload/v1515782149/cx6yn9xzfc1phowym2oa.png" alt="Dosh on iPhone X" id="iPhoneX" />
+				<img src="http://res.cloudinary.com/rocksauce-studios/image/upload/v1515782150/jhjswbxpzvewavcb5uxa.png" alt="Dosh on Pixel 2XL" id="pixel" />			
+			</div>
+			
+		<!-- Merchant Portal -->
+			<h2>Merchant Portal</h2>
+			<p>With a product like Dosh, you need a heavy-duty backend system to allow merchants, providers, and stores to craft specific offers, track analytics and see how redemptions are happening. Afterall, if the goal is to move marketing dollars directly into the pockets of consumers, then it's vital to know everything is working.</p>
+			
+			<div class="webScreens">
+				<img src="http://res.cloudinary.com/rocksauce-studios/image/upload/v1515782150/pmd85jirekn8lvy3limj.jpg" alt="Dosh Merchant Portal Dashboard" />
+				<img src="http://res.cloudinary.com/rocksauce-studios/image/upload/v1515782150/qshnz2yav8ytx9cfdagz.jpg" alt="Dosh Merchant Portal Offer Creation" />
+				<img src="http://res.cloudinary.com/rocksauce-studios/image/upload/v1515782150/bh0vozinf6woywzwxxn4.jpg" alt="Dosh Location Picker" />				
+			</div>			
+	</div>
+</div>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<script>
+	$(document).ready(function() {
+			var sliderImg = $(".sliderGraphic img");
+			var sliderTxt = $(".textMove");
+			
+			$('html').mousemove(function(event) {
+				var devX = event.pageX * 0.001;
+				var devY = event.pageY * 0.001;
+	
+				var textX = event.pageX * -0.002;
+				var textY = event.pageY * -0.002;
+				
+				var transformDev = 'translate3d(' + devX + '%, ' + devY + '%, 0)';
+				var transformText = 'translate3d(' + textX + '%, ' + textY + '%, 0)';
+				
+				sliderImg.css({
+					"transform" : transformDev
+				});
+				
+				sliderTxt.css({
+					"transform" : transformText				
+				});
+				
+				console.log(transformDev);
+			});
+		});
+</script>
+	
+<?php get_footer(); ?>
