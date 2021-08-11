98d9ed3903de1b327476f23c72759aa6f825d8df
mojayaute <colorrr@gmail.com>
2021-08-11 13:10:26 -0500

    porphit page

---
+++ work/page-prophit.php
@@ -0,0 +1,114 @@
+<?php
+/**
+* Template Name: Work - Prophit
+*
+*/
+?>
+
+<?php get_header(); ?>
+<link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/work/prophit/assets/css/prophit.css?version=<?php echo rand(1, 2000) ?>" type="text/css" />
+<div class="case-study-single">
+<!-- Top Piece -->
+	<div id="topImg"></div>
+	<div id="bigPhone"></div>
+<!-- /Top Piece -->
+<!-- Explanation -->
+	<div class="mainLeftSide descriptions">
+		<!-- Logo Explanations -->
+			<div class="textBox firstBox lightestBox borderTop">
+				<p>
+					Picking the right branding is vital to determining the style of the project. Once we had the name Prophit, there were a few directions we could go.
+				</p>
+			</div>
+			<div class="textBox borderTop lightestBox">
+				<p>
+					Since the name was decided early, we initially toyed with using magical imagery, such as genies and lamps. Cute, but it quite didn't fit our vision.
+				</p>
+			</div>		
+			<div class="textBox borderBoth lightestBox">
+				<p>
+					After testing sterile, more modern concepts, we wound up with a modified price tag, and a powerful geometric font treatment.
+				</p>
+			</div>		
+		<!-- /Logo Explanations -->		
+		<div class="spacerOne"></div>
+		<div class="textBox borderTop lightestBox">
+			<p>
+				We created Prophit to be the quick, go-to application for anyone needing to keep track of sales  & margins. 
+			</p>
+		</div>		
+		<div class="textBox borderBoth lightestBox">
+			<p>
+				Custom, retro-styled keyboard created to be reminiscent of classic desk calculators. Use the dedicated “next field” key to allow users to easily move through each form field as you enter items.
+			</p>
+		</div>	
+		<div class="spacerTwo"></div>		
+		<div class="textBox borderTop lightestBox">
+			<p>
+				Simple, clean, data entry to let you quickly enter your item details, and get back to running your business.
+			</p>
+		</div>		
+		<div class="textBox borderBoth lightestBox">
+			<p>
+				Easily test out different combinations of mark-up and margin, to maximize profit for the number of items on hand. 
+			</p>
+		</div>				
+	</div>
+	<div class="mainRightSide illustrations">
+		<!-- Logo Details -->
+			<table id="logoTable">
+				<tr>
+					<td><img src="http://res.cloudinary.com/rocksauce-studios/image/upload/v1419703610/bfypguaz1lj9akbq4wbn.png" alt="Prophit Lamp Logo" title="Prophit Lamp Logo"/></td>
+					<td><img src="http://res.cloudinary.com/rocksauce-studios/image/upload/v1419703609/b8tbx8gcyysbsromp6or.png" alt="Prophit Genie Logo" title="Prophit Genie Logo" /></td>					
+				</tr>				
+				<tr>
+					<td><img src="http://res.cloudinary.com/rocksauce-studios/image/upload/v1419703609/pegpp52vwpw9dcsddaiy.png" alt="Prophit P" title="Prophit P"/></td>
+					<td><img src="http://res.cloudinary.com/rocksauce-studios/image/upload/v1419703610/ygdxccftazgspizel63o.png" alt="Prophit Elegant" title="Prophit Elegant" /></td>					
+				</tr>	
+				<tr>
+					<td colspan="2" class="finalLogo">
+						<img src="http://res.cloudinary.com/rocksauce-studios/image/upload/v1419703610/bu68gw8pztl4bctisnuh.png" alt="Prophit Final Logo" title="Prophit Final Logo" />
+					</td>
+				</tr>			
+			</table>
+		<!-- /Logo Details -->	
+		<!-- Keypad -->
+			<div id="keyPad">
+				<img src="http://res.cloudinary.com/rocksauce-studios/image/upload/v1419703610/ozjhmvlalfxmbklwdv8j.png" alt="Prophit Numeric Keypad" title="Prophit Numberic Keypad"/>
+			</div>
+		<!-- /Keypad -->	
+		<!-- Entry Fields -->
+			<div id="entryFields"></div>
+		<!-- /Entry Fields -->
+	</div>
+	<div class="longDivider"></div>
+	<div id="shopPhoto">
+		<!-- Screenshots -->
+			<div class="mainLeftSide screenShots">
+				<div id="screenOne"></div>
+				<div id="screenTwo"></div>				
+			</div>
+			<div class="mainRightSide behindPhotos">
+				<div class="onPhotoText boxOne">
+					<p>Storefronts are easy to view and sort through, either by timeline or by date created. Search through the items, see what the costs are, and what the total retail prices are.</p>
+				</div>
+				<div class="onPhotoText boxTwo">
+					<p>Sales can be loged directly into Prophit, working as a quick inventory & profit management center. Reports allow deep views of what sold, plus how much was made. Then exported & emailed as a .CSV for bookkeeping.</p>
+				</div>
+			</div>
+		<!-- /Screenshots -->
+	</div>
+	<div id="quoteArea">
+		<div class="photo"></div>
+		<div class="text">
+			<p>"It was an awesome experience watching an idea grow in to something so great with the Rocksauce Studios team."</p>
+			<h1>Micah Payne, <strong>Prophit Creator</strong></h1>
+		</div>	
+	</div>
+	<div class="plusSign plusOne"></div>
+	<div class="plusSign plusTwo"></div>
+	<div class="plusSign plusThree"></div>
+<!-- /Explanation -->
+</div>
+	
+<?php get_footer(); ?>
