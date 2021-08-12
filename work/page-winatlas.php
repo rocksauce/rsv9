7bfa08d90488a9e2cca88e787fb00708c2e58f6a
mojayaute <colorrr@gmail.com>
2021-08-12 17:15:48 -0500

    win at last

---
+++ work/page-winatlas.php
@@ -0,0 +1,153 @@
+<?php
+/**
+* Template Name: Work - Winatlas
+*
+*/
+?>
+
+<?php get_header(); ?>
+<link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/work/winatlas/assets/css/win.css?version=<?php echo rand(1, 2000) ?>" type="text/css" />
+<div class="case-study-single">
+		<div class="top">
+			<div class="logo">
+				<img src="http://res.cloudinary.com/rocksauce-studios/image/upload/v1441059937/fko0d6hiudqmuh5oothp.png" alt="WIN Atlas" title="WIN Atlas" />							
+			</div>
+		</div>
+		<div class="description">
+			<text class="center">
+				<div class="icons">
+					<img src="http://res.cloudinary.com/rocksauce-studios/image/upload/v1441059937/nivxb0md5oclnofcyzpy.png" alt="WIN Atlas Icons" type="WIN Atlas Icons" />
+				</div>
+				<h1>WHAT WILL YOU BE WHEN YOU GROW UP?</h1>
+				<p>Remember being a teenager? Peer pressure from everyone around you. Dealing with homework, friend's needs, and always that drive to be cool no matter the situation. On top of it all,
+adulthood loomed right around the corner waiting for you.</p>
+
+				<p>To figure out your future, all you had at your disposal were pages of endless surveys or brief meetings with a guidance counselor who had to divide their time up between hundreds of other needy students.</p>
+
+				<p class="emphasis">WIN Learning came to Rocksauce Studios to help fix all of that.<br /><span class="call">We helped them create WIN Atlas.</span></p>
+			</text>
+		</div>
+		<div class="ux">
+			<div class="area">
+				<text>
+					<h1>GETTING STARTED IS THE HARDEST PART</h1>
+					<p>We were all teenagers once, too. Every generation from this point forward will live & breathe mobile. Want them to use a tool? You've gotta' make it engaging, simple and enjoyable.</p>
+
+					<p>But how do you accomplish that when some teens are planners, who have worked hard & planned through school but may not know what the next step is, while also accounting for others who have never thought past their high school graduation day?</p>
+
+					<p>For those that knew what they wanted, we let them choose their paths. Others, who were clueless on their future, were offered quick ways to get the information they were craving. We created User Personas for each teen type, so we could better pinpoint how best to communicate with them.</p>
+
+					<p>Research verified our expectations - teens wanted information quickly and to get something useful as soon as possible. Based on their User Persona, we helped each get arrive there in a way that was customized for them.</p>
+
+					<p class="emphasis">Our goal was to give students numerous paths to discovery, so they were able to choose a method which worked for them, no matter their future career.</p>
+				</text>
+				<div class="uxImage">
+					<img src="http://res.cloudinary.com/rocksauce-studios/image/upload/v1441059938/xvygdjkwcxq0pngtxpmo.png" alt="WIN Atlas UX Examples" title="WIN Atlas UX Examples" />
+				</div>
+			</div>
+		</div>
+		<div class="branding">
+			<text class="center">
+				<h1>DEFINING A BRAND THAT TEENS WILL ACTUALLY END UP USING</h1>
+				<p>Our client, WIN Learning, knew that for a teen facing their future, the idea of what comes next was the biggest decision they could make. Literally, these teens have the weight of the world on their shoulders. They also needed a "guide" to help them arrive at their eventual career destination. When it came time for naming, WIN Atlas was the perfect name.</p>
+			</text>
+			<div class="logoArea">
+				<div class="theLogo"><img src="http://res.cloudinary.com/rocksauce-studios/image/upload/v1441059937/elzw0ugkjw42hjbjrrpo.png" alt="WIN Atlas Logo Version 1" title="WIN Atlas Logo Version 2" /></div>
+				<div class="explain">
+					<p class="one smaller">Here we played with the simple concept of a compass, but used the type from a geometry class - playing both on the educational aspect and the navigational angle.</p>
+				</div>
+			</div>
+			<div class="logoArea">
+				<div class="theLogo"><img src="http://res.cloudinary.com/rocksauce-studios/image/upload/v1441059938/wcw8qqxk0o9e06rwwglx.png" alt="WIN Atlas Logo Version 2" title="WIN Atlas Logo Version 2" /></div>
+				<div class="explain">
+					<p class="two smaller">A globe symbolized all three of the primary meanings for Atlas - navigational tool, educational assistant, and of course, the world itself.</p>
+				</div>
+			</div>	
+			<div class="logoArea">
+				<div class="theLogo"><img src="http://res.cloudinary.com/rocksauce-studios/image/upload/v1441059937/heweia3eviefokscxxou.png" alt="WIN Atlas Logo Version 3" title="WIN Atlas Logo Version 3" /></div>
+				<div class="explain">
+					<p class="three smaller">For these versions, our team wanted to showcase the concept of Atlas holding up the world. Around the planet, we incorporated paths of an atom, bringing in the concept of future thinking.</p>
+					<p class="smaller">In the chosen logo, we tied the type treatment & the concept of Atlas together, creating one powerful, distinctive mark.</p>
+				</div>
+			</div>
+		</div>
+		<div class="elements">
+			<text class="center">
+				<h1>CREATING TAP ENTICEMENT & INSPIRING CAREER DISCOVERY</h1>
+				<p>With the UX planned and our branding defined, we next had to figure out a strong visual identity. Aside from color palette, which you can see throughout this case study, landing on a specific style for the iconography was vital. Driving users to tap, explore, and discover a new career path was our primary goal.</p>
+			</text>
+			<div class="icons"><img src="http://res.cloudinary.com/rocksauce-studios/image/upload/v1441059938/cq7ebtsw06osorrjlv4m.png" alt="WIN Atlas Icons" title="WIN Atlas Icons" /></div>
+			<text class="center">
+				<p class="smaller">After trying out a few different styles, we settled on a simple, outlined icon with an offset "print" design that was fun, distinctive, and would be familiar to the user. From the navigational icons on each page to the defining elements for specific careers, we wanted to make sure students had something easy to remember as they moved through the tool.</p>
+			</text>
+			<div class="icons"><img src="http://res.cloudinary.com/rocksauce-studios/image/upload/v1441059938/nwa73sssrevoeret7pjq.png" alt="WIN Atlas User Interface Elements" title="WIN Atlas User Interface Elements" /></div>
+			<text class="center">
+				<p class="smaller">We loved the iconography so much, we thought, "Heck, let's apply this style to the rest of the application, too!" So that's what we did. From the various different form fields that users interact with, to the buttons, progress bars and major actions, this style added a sense of fun everywhere. </p>
+			</text>
+		</div>
+		
+		<div class="screenArea">
+			<div class="frame">
+				<ul class="slidee_container">
+					<li class="slidee clickToView"></li>
+					<li class="slidee shot1"></li>
+					<li class="slidee shot2"></li>
+					<li class="slidee shot3"></li>
+					<li class="slidee shot4"></li>
+					<li class="slidee shot1"></li>
+					<li class="slidee shot2"></li>
+					<li class="slidee shot3"></li>
+					<li class="slidee shot4"></li>
+					<li class="slidee shot1"></li>
+					<li class="slidee shot2"></li>
+					<li class="slidee shot3"></li>
+					<li class="slidee shot4"></li>	
+					<li class="slidee clickToView"></li>				
+				</ul>
+			</div>
+		</div>
+		<div class="quoteArea">
+			<text>
+				<div class="logo"><img src="http://res.cloudinary.com/rocksauce-studios/image/upload/v1441059937/uuvatjqqgvltul3beazo.png" alt="WIN Learning Logo" title="WIN Learning Logo" /></div>
+				<p>"We're coming back to Rocksauce (for our website) because your design is stunning and beautiful."</p>
+				<p class="person">Suzanne Bazarian, WIN Learning</p>
+			</text>	
+		</div>
+	</div>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+	<script src="<?php echo get_stylesheet_directory_uri(); ?>/work/js/sly.min.js"></script>
+	<script>
+	    (function () {
+	    		var $frame = $('.frame');
+	    		var $wrap  = $frame.parent();
+
+	    		// Call Sly on frame
+	    		$frame.sly({
+	    			horizontal: 1,
+	    			itemNav: 'centered',
+	    			smart: 1,
+	    			activateMiddle: 1,
+	    			activateOn: 'click',
+	    			mouseDragging: 1,
+	    			touchDragging: 1,
+	    			releaseSwing: 1,
+	    			startAt: 0,
+	    			scrollBy: 1,
+	    			speed: 300,
+	    			elasticBounds: 1,
+	    			dragHandle: 1,
+	    			dynamicHandle: 1,
+	    			clickBar: 1
+	    		});
+	    	}());
+	    	
+	    	
+	    	$( document ).ready(function() {
+				$(window).resize(function(e) {
+			        $frame.sly('reload');
+				});
+			});
+	    	 
+    </script>	
+	
+<?php get_footer(); ?>
